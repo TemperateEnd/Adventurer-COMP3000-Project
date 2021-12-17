49d34f9d74de45fa403e97d5c7b5a57457b42f64
Reece Tarrant <rtarrant70998@gmail.com>
2021-12-17 17:52:23 +0000

    Sprint 4 - Commit 17/12/2021 @ 17:52
    Implemented Potion and Food scriptable objects

---
+++ Adventurer/Assets/Resources/Scripts/ScriptableObjects/Inventory/Potion.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : Consumable
+{
+    public Potion()
+    {
+        this.typeOfConsumable = consumableType.Potion;
+    }
+}
