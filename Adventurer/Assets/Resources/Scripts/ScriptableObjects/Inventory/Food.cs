cab07b7b58fabb44e7faaaab404e877eca108ae9
Reece Tarrant <rtarrant70998@gmail.com>
2021-12-21 00:38:41 +0000

    Sprint 4 - Commit 21/12/2021 @ 00:38
    Worked more on Food and Potion scriptableobjects.

--- Adventurer/Assets/Resources/Scripts/ScriptableObjects/Inventory/Food.cs
+++ Adventurer/Assets/Resources/Scripts/ScriptableObjects/Inventory/Food.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Food", menuName = "Adventurer/Item/Consumable/Food")]
 public class Food : Consumable
 {
+    public int staminaRestoreCount;
+    public int healthRestoreCount;
     public Food()
     {
         this.typeOfConsumable = consumableType.Food;
--- Adventurer/Assets/Resources/Scripts/ScriptableObjects/Inventory/Potion.cs
+++ Adventurer/Assets/Resources/Scripts/ScriptableObjects/Inventory/Potion.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Potion", menuName = "Adventurer/Item/Consumable/Potion")]
 public class Potion : Consumable
 {
+    public int restoreAmount;
     public Potion()
     {
         this.typeOfConsumable = consumableType.Potion;
